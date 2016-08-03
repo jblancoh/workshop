09a331bbbfda07103ab042198c879fb0b19b6b0f
jblancoh <zlatanjon@hotmail.com>
2016-08-03 13:03:07 -0500

    add solution array 02

--- 02-array/solucion.1-2.js
+++ 02-array/solucion.1-2.js
@@ -8,26 +8,47 @@
   //  Array (Array con Cadena de caracteres)
 // PSEUDOCODE
   //Convertir cadena de caracteres a un arreglo.
+  //Realizar un bucle para separar cada elemento del arreglo
+  //se compara cada iteracion para determinar numeros y letras
   //utilizar metodo splice para quitar elementos
   //imprimir el resultado en un array
 
-  //
-  // the condition is decremented in each iteration
-  // print the result of the string in correct order.
+  // Convert string to array
+  // Make a loop to separate each array element.
+  // Each iteration is compared to determine numbers and letters
+  // Use splice method to merove items
+  // print the result in an array
+
+// 'use strict';
+// let mensaje = 'S23984203948023948234o34234345453y52345435345 345345345345345345u3453453453223452452345n324534545346654768780807896784865 535624643531453245234154g24366464565346r645634564576573a4573435735468n45684568 456d845e684658654867497465732456s246534a7567r456r74568465865865o86l7845685735743573457354735l67645865478564864584658658a45685484658456d456845684684563562453754o73573457437567345635688r';
+//
+// let resultado = [];
+//
+// for(var i=0 ;i<mensaje.length;i++){
+//       if(isNaN(mensaje[i]) === true)  {
+//         resultado += mensaje[i];
+//       }
+// }
+// console.log(resultado);
+//
+
+//REFACTORIZAR
+
+
 'use strict';
 let mensaje = 'S23984203948023948234o34234345453y52345435345 345345345345345345u3453453453223452452345n324534545346654768780807896784865 535624643531453245234154g24366464565346r645634564576573a4573435735468n45684568 456d845e684658654867497465732456s246534a7567r456r74568465865865o86l7845685735743573457354735l67645865478564864584658658a45685484658456d456845684684563562453754o73573457437567345635688r';
-let a = parseInt(mensaje);
-let resultado = [];
-// var pos = letras.indexOf(numeros);
-console.log(a);
+let arrMensaje = mensaje.split('');
 
-for(var i=0 ;i<mensaje.length;i++){
-  resultado += mensaje[i];
-//       if(letras[i] != numeros)  {
-//         // console.log(letras.splice(pos,1));
-//           console.log(letras);
-//       }
+for(var i=0 ;i<arrMensaje.length;i++){
+
+      if(isNaN(parseInt(arrMensaje[i])) === false )  {
+
+        arrMensaje.splice(i,1);
+        i--;
+        // console.log('Este es el indice > '+i);
+        // console.log('Este es el valor > '+arrMensaje[i]);
+        // console.log('Esto se borro > '+arrMensaje.splice(i,1) );
+
+      }
 }
-console.log(mensaje[i]);
-// console.log(letras);
-// console.log(letras);
+console.log(' > '+ arrMensaje);
